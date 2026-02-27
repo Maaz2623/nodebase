4b82fca3fd07be0eb3288d19ffe4956f671917a0
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-02-27 18:27:33 +0530

    modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   prisma/migrations/20260227103442_auth_implementation/migration.sql 	modified:   prisma/schema.prisma 	new file:   src/app/(auth)/login/page.tsx 	new file:   src/app/(auth)/signup/page.tsx 	new file:   src/app/api/auth/[...all]/route.ts 	deleted:    src/app/client.tsx 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx 	new file:   src/features/auth/components/login-form.tsx 	new file:   src/features/auth/components/register-form.tsx 	new file:   src/lib/auth-client.ts 	new file:   src/lib/auth-utils.ts 	new file:   src/lib/auth.ts 	modified:   src/trpc/init.ts 	modified:   src/trpc/routers/_app.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import { caller, getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { requireAuth } from "@/lib/auth-utils";
+import React from "react";
 
 const HomePage = async () => {
-  const queryClient = getQueryClient();
+  await requireAuth();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-  return (
-    <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
-    </div>
-  );
+  return <div>Home Page</div>;
 };
 
 export default HomePage;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,5 @@
 import { baseProcedure, createTRPCRouter } from "../init";
 import prisma from "@/lib/db";
-export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query((opts) => {
-    return prisma.user.findMany();
-  }),
-});
+export const appRouter = createTRPCRouter({});
 // export type definition of API
 export type AppRouter = typeof appRouter;
