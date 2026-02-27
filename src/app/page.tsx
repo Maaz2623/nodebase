b21957a776eb681ae60b99f016ce5b79536a7bc2
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-02-27 15:38:12 +0530

    modified:   .gitignore 	modified:   package.json 	modified:   pnpm-lock.yaml 	renamed:    prisma/migrations/20260226130208_test_initial/migration.sql -> prisma/migrations/20260227095301_init/migration.sql 	modified:   prisma/schema.prisma 	modified:   src/app/page.tsx 	new file:   src/lib/db.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import prisma from "@/lib/db";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+const HomePage = async () => {
+  const users = await prisma.user.findMany();
+  return <div>{JSON.stringify(users)}</div>;
 };
 
 export default HomePage;
