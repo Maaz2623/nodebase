b3ca7cca92f40e54f03a417b613bd815c9d62e05
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-02-28 23:08:51 +0530

    new file:   mprocs.log 	new file:   mprocs.yaml 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   prisma/migrations/20260228164632_workflows_table/migration.sql 	modified:   prisma/schema.prisma 	new file:   src/app/api/inngest/route.ts 	modified:   src/app/page.tsx 	new file:   src/inngest/client.ts 	new file:   src/inngest/function.ts 	modified:   src/trpc/routers/_app.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { requireAuth } from "@/lib/auth-utils";
-import React from "react";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const HomePage = async () => {
-  await requireAuth();
+const HomePage = () => {
+  const trpc = useTRPC();
+
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const queryClient = useQueryClient();
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    }),
+  );
 
   return (
     <div>
-      <Button>Click Me</Button>
+      {JSON.stringify(data)}
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,23 @@
-import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async ({}) => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "mohammedmaaz2623@gmail.com",
+      },
+    });
+
+    return {
+      success: true,
+      message: "Job queued",
+    };
+  }),
+});
 // export type definition of API
 export type AppRouter = typeof appRouter;
