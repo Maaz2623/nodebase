0f0d3f5120ae852fe2bc43de95565f831686b3ee
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-03-04 00:13:55 +0530

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/components/app-sidebar.tsx 	new file:   src/features/subscriptions/hooks/use-subscription.ts 	modified:   src/lib/auth-client.ts 	modified:   src/lib/auth.ts 	new file:   src/lib/polar.ts 	modified:   src/trpc/init.ts

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -23,6 +23,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { authClient } from "@/lib/auth-client";
+import { useHasActiveSubsciption } from "@/features/subscriptions/hooks/use-subscription";
 
 const menuItems = [
   {
@@ -51,6 +52,8 @@ export const AppSidebar = () => {
   const router = useRouter();
 
   const pathname = usePathname();
+
+  const { isLoading, hasActiveSubsciption } = useHasActiveSubsciption();
 
   return (
     <Sidebar collapsible="icon">
@@ -95,21 +98,29 @@ export const AppSidebar = () => {
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              tooltip={`Upgrade To Pro`}
-              className="gap-x-4 h-10 px-4"
-              onClick={() => {}}
-            >
-              <StarIcon className="h-4 w-4" />
-              <span>Upgrade to PRo</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+          {!hasActiveSubsciption && !isLoading && (
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                tooltip={`Upgrade To Pro`}
+                className="gap-x-4 h-10 px-4"
+                onClick={() => {
+                  authClient.checkout({
+                    slug: "pro",
+                  });
+                }}
+              >
+                <StarIcon className="h-4 w-4" />
+                <span>Upgrade to PRo</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          )}
           <SidebarMenuItem>
             <SidebarMenuButton
               tooltip={`Billing Portal`}
               className="gap-x-4 h-10 px-4"
-              onClick={() => {}}
+              onClick={() => {
+                authClient.customer.portal();
+              }}
             >
               <CreditCardIcon className="h-4 w-4" />
               <span>Billing Portal</span>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,38 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import prisma from "./db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "d9636821-d85c-41f3-a09f-898045d0b10e",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal({
+          
+        })
+      ],
+    }),
+  ],
 });
