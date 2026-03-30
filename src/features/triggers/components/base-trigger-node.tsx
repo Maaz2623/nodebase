3abfb7096405254face6ed89608d5402a6ba4247
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-03-30 23:57:08 +0530

    modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   prisma/migrations/20260330153449_new_nodes/migration.sql 	modified:   prisma/schema.prisma 	modified:   src/components/initial-node.tsx 	new file:   src/components/node-selector.tsx 	new file:   src/components/react-flow/base-handle.tsx 	modified:   src/config/node-components.ts 	modified:   src/features/editor/components/add-node-button.tsx 	new file:   src/features/executions/components/base-execution-node.tsx 	new file:   src/features/executions/components/http-request/node.tsx 	new file:   src/features/triggers/components/base-trigger-node.tsx 	new file:   src/features/triggers/components/manual-trigger/node.tsx

---
+++ src/features/triggers/components/base-trigger-node.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
+import { BaseHandle } from "@/components/react-flow/base-handle";
+import { NodeProps, Position } from "@xyflow/react";
+import { LucideIcon } from "lucide-react";
+import { memo } from "react";
+import { WorkflowNode } from "../../../components/workflow-node";
+import Image from "next/image";
+
+interface BaseTriggerProps extends NodeProps {
+  icon: LucideIcon | string;
+  name: string;
+  description?: string;
+  children?: React.ReactNode;
+  //   status?: NodeStatus;
+  onSettings?: () => void;
+  onDoubleClick?: () => void;
+}
+
+export const BaseTriggerNode = memo(
+  ({
+    id,
+    icon: Icon,
+    name,
+    description,
+    children,
+    onSettings,
+    onDoubleClick,
+  }: BaseTriggerProps) => {
+    const handleDelete = () => {};
+
+    return (
+      <WorkflowNode
+        name={name}
+        description={description}
+        onSettings={onSettings}
+        onDelete={handleDelete}
+      >
+        <BaseNode
+          onDoubleClick={onDoubleClick}
+          className="rounded-l-2xl relative group"
+        >
+          <BaseNodeContent>
+            {typeof Icon === "string" ? (
+              <Image src={Icon} alt={name} width={16} height={16} />
+            ) : (
+              <Icon className="size-4 text-muted-foreground" />
+            )}
+            {children}
+            <BaseHandle id="source-1" type="source" position={Position.Right} />
+          </BaseNodeContent>
+        </BaseNode>
+      </WorkflowNode>
+    );
+  },
+);
+
+BaseTriggerNode.displayName = "BaseTriggerNode";
