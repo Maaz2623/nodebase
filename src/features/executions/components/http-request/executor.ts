9b6c774b09cf7c934f26ecf031ef723ac520a445
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-05-06 23:20:40 +0530

    modified:   mprocs.log 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   src/features/executions/components/http-request/actions.ts 	modified:   src/features/executions/components/http-request/executor.ts 	modified:   src/features/executions/components/http-request/node.tsx 	new file:   src/features/executions/hooks/use-node-status.ts 	modified:   src/features/executions/types.ts 	new file:   src/features/triggers/components/manual-trigger/actions.ts 	modified:   src/features/triggers/components/manual-trigger/executor.ts 	modified:   src/features/triggers/components/manual-trigger/node.tsx 	new file:   src/inngest/channels/http-request.ts 	new file:   src/inngest/channels/manual-trigger.ts 	modified:   src/inngest/client.ts 	modified:   src/inngest/function.ts

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -2,6 +2,7 @@ import { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 import Handlebars from "handlebars";
+import { httpRequestChannel } from "@/inngest/channels/http-request";
 
 type HttpRequestData = {
   endpoint: string;
@@ -22,55 +23,98 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   nodeId,
   context,
   step,
+  publish,
 }) => {
+  await publish(
+    httpRequestChannel().status({
+      nodeId,
+      status: "loading",
+    }),
+  );
+
   if (!data.endpoint) {
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
     throw new NonRetriableError("HTTP Request node: No endpoint configured");
   }
 
   if (!data.variableName) {
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
     throw new NonRetriableError("Variable name not configured");
   }
 
   if (!data.method) {
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
     throw new NonRetriableError("Method not configured");
   }
+  await step.sleep("delay", "10s");
+  try {
+    const result = step.run("http-request", async () => {
+      const endpoint = Handlebars.compile(data.endpoint)(context);
+      const method = data.method || "GET";
 
-  const result = step.run("http-request", async () => {
-    const endpoint = Handlebars.compile(data.endpoint)(context);
-    const method = data.method || "GET";
-
-    const options: KyOptions = {
-      method: method,
-    };
-
-    if (["POST", "PUT", "PATCH"].includes(method)) {
-      const resolved = Handlebars.compile(data.body)(context);
-      JSON.parse(resolved);
-      options.body = resolved;
-      options.headers = {
-        "Content-Type": "application/json",
+      const options: KyOptions = {
+        method: method,
       };
-    }
 
-    const response = await ky(endpoint, options);
-    const contentType = response.headers.get("content-type");
-    const responseData = contentType?.includes("application/json")
-      ? await response.json()
-      : await response.text();
+      if (["POST", "PUT", "PATCH"].includes(method)) {
+        const resolved = Handlebars.compile(data.body)(context);
+        JSON.parse(resolved);
+        options.body = resolved;
+        options.headers = {
+          "Content-Type": "application/json",
+        };
+      }
 
-    const responsePayload = {
-      httpResponse: {
-        status: response.status,
-        statusText: response.statusText,
-        data: responseData,
-      },
-    };
+      const response = await ky(endpoint, options);
+      const contentType = response.headers.get("content-type");
+      const responseData = contentType?.includes("application/json")
+        ? await response.json()
+        : await response.text();
 
-    return {
-      ...context,
-      [data.variableName]: responsePayload,
-    };
-  });
+      const responsePayload = {
+        httpResponse: {
+          status: response.status,
+          statusText: response.statusText,
+          data: responseData,
+        },
+      };
 
-  return result;
+      return {
+        ...context,
+        [data.variableName]: responsePayload,
+      };
+    });
+
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "success",
+      }),
+    );
+
+    return result;
+  } catch (error) {
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
+    throw error;
+  }
 };
